314542ce4cc43856c9c83ea86302a9fe84028665
A_sc0rp <127619163+A-sc0rp@users.noreply.github.com>
2024-02-28 20:36:13 +0100

    Delete Python_shit/ScreenSaver_kinda/window_script.bat

--- Python_shit/ScreenSaver_kinda/window_script.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-:loop
-C:\Users\Utente\AppData\Local\Microsoft\WinGet\Packages\NirSoft.NirCmd_Microsoft.Winget.Source_8wekyb3d8bbwe\nircmd.exe closeprocess Python.exe
-timeout /nobreak /t 1 >null
-goto loop
